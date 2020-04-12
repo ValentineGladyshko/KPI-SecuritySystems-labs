a0c96cb46540c53f71c521b5e430baef92678996
Valentine <valentine9022@gmail.com>
2020-04-12 11:42:03 +0300

    Added new project

---
+++ SecuritySystemLab1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SecuritySystemLab1.Startup))]
+namespace SecuritySystemLab1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
